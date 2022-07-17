a6607b43c1f192ee970b5bef122316d3314aea94
TurMikolaj <mikolajturowski@gmail.com>
2022-07-17 14:00:36 +0200

    WA-001 create UserInterface

--- src/main/java/com/sda/Application.java
+++ src/main/java/com/sda/Application.java
@@ -5,6 +5,8 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Michal tu byl");
+        UserInterface userInterface = new UserInterface();
+        userInterface.run();
     }
 
 }
